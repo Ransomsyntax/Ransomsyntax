1713c1fccc66884b42787d287617d4232418623c
Ransomsyntax <ransomsyntax@gmail.com>
2026-08-01 12:42:53 +0530

    Configure PostgreSQL for Vercel

--- config/settings.py
+++ config/settings.py
@@ -119,13 +119,29 @@ ASGI_APPLICATION = "config.asgi.application"
 # Database
 # ---------------------------------------------------------------------------
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import os
+from urllib.parse import urlparse
 
+if os.environ.get("POSTGRES_URL"):
+    url = urlparse(os.environ["POSTGRES_URL"])
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": url.path[1:],
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port,
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # ---------------------------------------------------------------------------
 # Authentication
 # ---------------------------------------------------------------------------
